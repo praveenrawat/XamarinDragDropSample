6e4858c614b038f2c5efc00feeb6fef43f0a22ef
Praveen Rawat <praveenrawat@unknown.home>
2021-04-25 22:03:35 +0100

    DragandDropSampleCode

---
+++ DragDropSample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DragDropSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void DragGestureRecognizer_DragStarting(System.Object sender, Xamarin.Forms.DragStartingEventArgs e)
+        {
+            //write your code
+        }
+
+        
+
+        void DropGestureRecognizer_DragOver(System.Object sender, Xamarin.Forms.DragEventArgs e)
+        {
+            var txt = e.Data?.Text;
+            var frame = (sender as Element)?.Parent as Frame;
+
+            frame.Content = new Label { Text = txt };
+        }
+    }
+}
